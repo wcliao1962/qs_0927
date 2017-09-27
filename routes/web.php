ddf032235ecfdd319f2dc80f64e486532bdd46d9
lwc <wcliao1962@gmail.com>
2017-09-27 13:45:06 +0800

    新增任務&驗證

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,22 @@ use Illuminate\Http\Request;
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // 建立該任務...
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
     });
 
     /**
